2efdbe3679cb24fe0b8e779edc87b362be4a17a1
cinqueterreli <Anthony@Anthonys-MBP.home>
2017-03-16 19:02:34 -0400

    weeee

--- js/main.js
+++ js/main.js
@@ -35,11 +35,11 @@ console.log(time);
 if (time < 16){
 	$('#target').load('/lunch-menu.html');
   $('#lunch').addClass('activated');
-  $("#menu-select").val(lunchValue).find("option[value=" + lunchValue +"]").attr('selected', true);
+  $("#menu-select").val('1');
 } else if (time >= 16){
 	$('#target').load('/dinner-menu.html');
   $('#dinner').addClass('activated');
-  $("#menu-select").val(dinnerValue).find("option[value=" + dinnerValue +"]").attr('selected', true);
+  $("#menu-select").val('2')
   if (window.matchMedia('(max-width: 870px)').matches){
     if (window.matchMedia('(min-width: 350px)').matches){
       $('#menu').css('min-height','5250px');
